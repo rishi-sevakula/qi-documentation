7434c9ccce1f2e2f90df57286397e8034b6304f8
rishi sevakula <rishi.sevakula@gep.com>
2026-01-21 20:25:43 +0530

    Update Docusaurus configuration for GitHub Pages deployment
    - Changed the site URL to 'https://rishi-sevakula.github.io'.
    - Updated the base URL to '/qi-documentation/' and enabled trailing slashes for improved routing.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,11 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://gep.com',
+  url: 'https://rishi-sevakula.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/qi-documentation/',
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
